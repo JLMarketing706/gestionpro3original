ec54581a4e60c57939dc911f19e5ef6413352619
Juan Ignacio <150470787+JLMarketing706@users.noreply.github.com>
2025-07-30 13:07:49 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
@@ -18,16 +17,6 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [
       react(),
-     VitePWA({
-  registerType: 'autoUpdate',
-  injectRegister: false, // Evita que el plugin registre el SW por su cuenta
-  manifest: false,
-  includeAssets: ['manifest.webmanifest'],
-  strategies: 'injectManifest', // No genera un SW automático
-  srcDir: 'public',
-  filename: 'service-worker.js',
-}),
-
     ],
   };
 });
